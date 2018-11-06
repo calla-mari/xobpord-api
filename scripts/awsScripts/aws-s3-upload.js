819218eba774d4f7208c417ff9668b30eba9c9b2
Mary Abraham and Zack Clark <maryabraham0125@gmail.com+zclark0625@gmail.com>
2018-11-06 11:07:49 -0500

    Added Upload Shell Script
    - added a sample png in the data folder
    - copypasta aws-s3-upload from trainings

---
+++ scripts/awsScripts/aws-s3-upload.js
@@ -0,0 +1,58 @@
+'use strict'
+
+// load config from .env file
+require('dotenv').config()
+// use Amazon's Express AWS SDK
+const AWS = require('aws-sdk')
+// use filestream to send the data to S3
+const fs = require('fs')
+// use mime to find the correct data types and extensions
+const mime = require('mime-types')
+
+// instantiate S3 object
+const S3 = new AWS.S3()
+
+// s3Upload will accept a path and name to upload to S3
+function s3Upload(filePath, fileName) {
+    // if a name is not given by the user, 'default' will be used instead
+    const name = fileName || 'default'
+    // create stream from the given file
+    const stream = fs.createReadStream(filePath)
+    // file type of the given file
+    const contentType = mime.lookup(filePath)
+    // extension of the given file
+    const ext = mime.extension(contentType)
+    // params defined by the aws-sdk
+    const params = {
+        // allows the file to be read/downloaded by anyone possessing the link
+        ACL: 'public-read',
+        // bucket to which the file should be uploaded, defined in .env
+        Bucket: process.env.BUCKET_NAME,
+        // name of the file in S3
+        Key: `${name}.${ext}`,
+        // content of the upload
+        Body: stream,
+        // file type of the file, so S3 knows how to handle the file
+        ContentType: contentType
+    }
+
+    // Promisify S3.upload
+    return new Promise((resolve, reject) => {
+        // pass the sdk our params and a callback to handle errors and amazon's return data
+        S3.upload(params, (err, data) => {
+            if (err) {
+                // reject on error
+                reject(err)
+            } else {
+                // resolve if no error, passing on the data returned from Amazon
+                resolve(data)
+            }
+        })
+    })
+}
+
+// call the function with the third and fourth arguments passed form the command line
+s3Upload(process.argv[2], process.argv[3])
+    // console.log the data if the upload resolves
+    .then(console.log)
+    .catch(console.error)
