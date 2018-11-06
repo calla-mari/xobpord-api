70da916024a1bcef5e6439084e4672d4f40d7435
Mary Abraham and Zack Clark <maryabraham0125@gmail.com+zclark0625@gmail.com>
2018-11-06 13:16:28 -0500

    Finished upload Model

---
+++ app/models/upload.js
@@ -0,0 +1,26 @@
+// use mongoose for db access
+const mongoose = require('mongoose')
+
+const uploadSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }, 
+    tags: {
+        type: [String],
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Upload', uploadSchema)
