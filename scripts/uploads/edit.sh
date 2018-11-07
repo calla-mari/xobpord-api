3607cd9fac71f2547103b307be15429d264de818
Kay Callahan and Troy Oram <marissa.callahan@gmail.com+oram0720@gmail.com>
2018-11-07 17:21:51 -0500

    add /ID to edit.sh

--- scripts/uploads/edit.sh
+++ scripts/uploads/edit.sh
@@ -3,7 +3,7 @@
 API="http://localhost:4741"
 URL_PATH="/uploads"
 
-curl "${API}${URL_PATH}" \
+curl "${API}${URL_PATH}/${ID}" \
   --include \
   --request PATCH \
   --header "Authorization: Bearer ${TOKEN}" \
